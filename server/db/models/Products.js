d6f8a6752f34d17ce39d7fea77d23df9d895a577
Bradley N. Plunkett <bradleyn.plunkett@Bradleys-MacBook-Pro.local>
2021-11-09 17:02:29 -0500

    pray for brad

--- server/db/models/Products.js
+++ server/db/models/Products.js
@@ -4,19 +4,22 @@ const db = require("../db");
 const Product = db.define("product", {
   name: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {notEmpty: true}
   },
   price: {
     type: Sequelize.DECIMAL,
+    allowNull: false,
+    validate: {min: 0}
   },
   stock: {
     type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {min: 0},
   },
   imageUrl: {
     type: Sequelize.STRING,
-  },
-  productId: {
-    type: Sequelize.INTEGER,
-  },
+  }
 });
 
 module.exports = Product;
