bfbc37066b1935ba9cbccf3f032543c5463e2d77
Chase Holt <chaseholt@orc3.com>
2021-11-10 13:03:04 -0500

    added api route to delete

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import AllProducts from "./components/AllProducts";
 
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 
 const App = () => {
+  const [secLevel, setSecLevel] = useState("customer");
+
   return (
     <div>
       <Navbar />
