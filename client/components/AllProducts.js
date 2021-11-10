4b05274b846d1be38fba1d956de0cb1f3f38fd90
scott <scottw91@live.ca>
2021-11-10 12:59:12 -0500

    added maybe a working cart????

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {connect, useSelector} from 'react-redux'
 
-export const AllProducts = () => {
+
+export const AllProducts = (props) => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios.get(`/api/products`).then((response) => {
       setData(response.data);
     });
   }, []);
+
+  const userId = useSelector(state => state.auth.id);
+
+  const handleCart = function(id){
+  console.log(id)
+
+  console.log(userId)
+  axios.put(`/api/products/${[id, userId]}`)
+  }
 
   const mappedProducts = data.map((product) => {
     return (
@@ -19,12 +30,18 @@ export const AllProducts = () => {
         <Link to={`/products/${product.id}`}>
           <button>View Product</button>
         </Link>
-        <button>Add to Cart</button>
+        <button id="addCart" type="button" onClick={() => handleCart(product.id)}>Add to Cart</button>
       </div>
     );
   });
 
   return <div>{mappedProducts}</div>;
 };
+
+const mapState = state => {
+  return {
+    userId: state.auth.id
+  }
+}
 
 export default AllProducts;
