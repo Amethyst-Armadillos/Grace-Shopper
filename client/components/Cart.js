fd910c912289f66a1c3f3862c7450393546ae87e
Bradley N. Plunkett <bradleyn.plunkett@Bradleys-MacBook-Pro.local>
2021-11-15 13:09:08 -0500

    added total price

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -130,11 +130,16 @@ export const Cart = () => {
   };
 
   let mappedCart;
+  let cartTotal = 0;
+  let bouquetCount = 0;
 
   if (cart) {
     if (cart.length != 0) {
       mappedCart = cart.map((product) => {
         console.log(product);
+        let productTotal = product.quantity* product.price
+        cartTotal += productTotal
+        bouquetCount+=product.quantity
         return (
           <div key={product.id} className='cart-items'>
             <div className='image-box'>
@@ -201,8 +206,8 @@ export const Cart = () => {
         <div className='total'>
           <div>
             <div className='subtotal'>Sub-Total</div>
-            <div className='items'>2 bouquets</div>
-            <div className='total-amount'>$6.00</div>
+            <div className='items'>{bouquetCount} bouquets</div>
+            <div className='total-amount'>${cartTotal.toFixed(2)}</div>
           </div>
         </div>
 
